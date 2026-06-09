168f43f43406b461162c0b73c7c7945382d9e980
Myra <myrabagga07@gmail.com>
2026-06-09 13:27:53 -0400

    updated src to be a package directory. updated embedder to class structure. added processor.py and main.py

--- app/embedder.py
+++ app/embedder.py
@@ -2,32 +2,17 @@ import os
 from dotenv import load_dotenv
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
-load_dotenv()
-hf_token = os.getenv("HF_TOKEN")
-model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2', token=hf_token)
 
-english = [
-    "The cat is on the table",
-    "It's a piece of cake.",
-    "Could you please confirm this?",
-    "Kill two birds with one stone.",
-    "Doing the homework was a piece of cake",
-]
+class VectorEngine:
+    def __init__(self, model_name='sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2'):
+        load_dotenv()
+        hf_token = os.getenv("HF_TOKEN")
+        self.model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2', token=hf_token)
+    def generate_embeddings(self, sentences):
+        embeddings = self.model.encode(sentences)
+        return embeddings
 
-japanese = [
-    "猫はテーブルの上にいます。",
-    "朝飯前です。",
-    "ご確認よろしくお願いします？",
-    "一石二鳥 ",
-    "宿題をするのは朝飯前だった",
-]
-
-embeddings1 = model.encode(english)
-embeddings2 = model.encode(japanese)
 
 # Compute cosine similarities
-similarities = cosine_similarity(embeddings1, embeddings2)
+# similarities = cosine_similarity(embeddings1, embeddings2)
 
-for idx_i, sentence1 in enumerate(english):
-    print(sentence1)
-    print(f" - {japanese[idx_i]}: {similarities[idx_i][idx_i]:.4f}")

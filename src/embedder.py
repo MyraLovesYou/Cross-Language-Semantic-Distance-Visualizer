0439999ae04d6de242302e51394d53fda5430af8
Myra <myrabagga07@gmail.com>
2026-06-14 20:42:57 -0400

    refactor: update .gitignore and requirements, remove dotenv dependency from embedder

--- src/embedder.py
+++ src/embedder.py
@@ -1,12 +1,10 @@
 import os
-from dotenv import load_dotenv
 from sentence_transformers import SentenceTransformer
-
+import streamlit as st
 
 class VectorEngine:
     def __init__(self, model_name='sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2'):
-        load_dotenv()
-        hf_token = os.getenv("HF_TOKEN")
+        hf_token = st.secrets["HF_TOKEN"]
         self.model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2', token=hf_token)
     def generate_embeddings(self, sentences):
         embeddings = self.model.encode(sentences)
